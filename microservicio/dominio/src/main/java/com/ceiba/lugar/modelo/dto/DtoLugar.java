94f402463aa941a92b1645c6d814fb6d73b22477
Andres Felipe Campaz <dtmo96@outlook.com>
2022-04-25 05:12:51 -0500

    prueba

--- microservicio/dominio/src/main/java/com/ceiba/lugar/modelo/dto/DtoLugar.java
+++ microservicio/dominio/src/main/java/com/ceiba/lugar/modelo/dto/DtoLugar.java
@@ -1,11 +1,30 @@
 package com.ceiba.lugar.modelo.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
 public class DtoLugar {
     private Long idLugar;
     private String estado;
+
+    public Long getIdLugar() {
+        return idLugar;
+    }
+
+    public void setIdLugar(Long idLugar) {
+        this.idLugar = idLugar;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public DtoLugar(Long idLugar, String estado) {
+        this.idLugar = idLugar;
+        this.estado = estado;
+    }
 }
